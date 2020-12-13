831883b0fd7b7dcebb95c355e3014914c6475466
Tai-4 <ka1ntrk@gmail.com>
2020-12-13 19:55:12 +0900

    たしゅぼっと
    First push.

---
+++ たしゅぼっと/Code/body.rb
@@ -0,0 +1,137 @@
+require 'discordrb'
+require '.\たしゅぼっと\Code\commands_help.rb'
+
+@bot = Discordrb::Commands::CommandBot.new(token: "Nzg0NzczODQ4Njg4MDk5Mzgw.X8uLmQ.yCtmd_ZD37Vdm7mh0MmrupeSUCY", client_id: 784773848688099380, prefix:'?')
+
+# 自己紹介チャンネルにおいて、規定に沿っていないメッセージを自動削除する
+# await!メソッドで永遠に待機させる必要があるので、マルチスレッド機能を利用する。
+Thread.new do
+  message_list = []
+  self_introduction_channel_id = 784702508927811604
+  self_introduction_channel = @bot.channel(self_introduction_channel_id, 784700980381351936) # Cacheを使用して自己紹介チャンネルのオブジェクトを生成。[引数: (チャンネルID, サーバーID) ]
+
+  Thread.new do
+    loop { message_list << self_introduction_channel.await!.message } # Message(Object)が戻り値。messageメソッドを使用しない場合は、Discordrb::Events::MessageEventというクラスのインスタンスが返される。
+  end
+
+  loop do
+    if message_list.empty?
+      sleep 3
+      next
+    end
+
+    delete_messages = message_list.reject { |message| /【ニックネーム】.*\n【ゲームタグ】.*\n【一言】.*/.match?(message.content) }
+    message_list.shift(delete_messages.size)
+    next if delete_messages.empty?
+
+    delete_messages_ids = delete_messages.map(&:id)
+    delete_messages_authors_mention = delete_messages.map { |delete_message| "<@#{delete_message.author.id}>"}.uniq
+    delete_messages_ids << @bot.send_message(self_introduction_channel_id, "#{delete_messages_authors_mention.join(" ")} 自己紹介以外のメッセージは禁止されています！").id
+    # send_messageの戻り値は送信したメッセージのMessage(Object)なので、idメソッドを呼び出せる。
+
+    sleep 5
+    Discordrb::API::Channel.bulk_delete_messages(@bot.token, self_introduction_channel_id, delete_messages_ids)
+    delete_messages.clear
+    delete_messages_ids.clear
+    delete_messages_authors_mention.clear
+  end
+end
+
+# 実験: 別ファイルに処理を記述して、実行させることは可能だろうか。 => 可能
+@bot.command :help do |event|
+  command_name = event.message.content.split[1]
+  commands_help(event.channel, command_name)
+end
+
+# command定義においては、ブロックの戻り値がコマンドが送信されたチャンネルに送信されることを利用することでコマンドが送信されたチャンネルにメッセージを送信することを実現している箇所もあります。
+@bot.command :cmsg do |event|
+  begin
+    target_message_id = event.message.content.split[1]
+    target_message_id ? Discordrb::API::Channel.message(@bot.token, event.channel.id, target_message_id) : "入力形式が違うよ。helpコマンドで確認してね。"
+  rescue RestClient::NotFound => e
+    "指定したメッセージは見つからなかったよ。"
+  end
+end
+
+@bot.command :name do |event|
+  name = event.author.username         # event.user.name も可(エイリアス)
+  nickname = event.author.nickname     # event.user.nick も可(エイリアス)
+  %(```Your username: #{name}\nYour nickname: #{nickname ? "#{nickname}": "None" }```)
+end
+
+@bot.command :addm do |event|
+  music_title = event.message.content.split[1]
+  unless music_title
+    return "曲名は必ず指定 ＼_(・ω・`)ココ重要！"
+  end
+
+  File.open('D:\ドキュメント\Programming\Project\Discord\Bot\たしゅぼっと\Add_music', "a+") do |file|
+    music_list = file.readlines.map(&:chomp)
+    if music_list.include?(music_title)
+      return "「#{music_title}」は既に追加されています..."
+    else
+      file.puts music_title
+      "「#{music_title}」の追加が完了しました！"
+    end
+  end
+end
+
+@bot.command :chom do |event|
+  begin
+    message_content = event.message.content
+    number = Integer(message_content.split[1])
+    permission = message_content.split[2]
+  rescue ArgumentError, TypeError
+    return "入力形式が違うよ。helpコマンドで確認してね。"
+  end
+
+  File.open('D:\ドキュメント\Programming\Project\Discord\Bot\たしゅぼっと\Tai4_Music', "r") do |music_file|
+    music_list = music_file.readlines
+    if permission == "on"
+      File.open('D:\ドキュメント\Programming\Project\Discord\Bot\たしゅぼっと\Add_music', "r") do |added_music_file|
+        added_music_file.readlines.each { |added_music| music_list << added_music}
+      end
+    end
+
+    if (1..(music_list.size)).include?(number)
+      message = ""
+      number.times do
+        target_music_index = rand 0...(music_list.size)
+        message << "#{music_list[target_music_index]}\n"
+        music_list.delete_at(target_music_index)
+      end
+      message
+    else
+      "現在指定できる曲数は、1~#{music_list.size}個です。"
+    end
+  end
+end
+
+@bot.command :dem do |event|
+  unless event.author.roles[0].permissions.bits.to_s(2)[-4] == "1" # 役職に管理者があるか確認(ビットフラグ)
+    return "おめーに管理者権限、ねぇから！"
+  end
+
+  begin
+    number = Integer(event.message.content.split[1])
+  rescue ArgumentError, TypeError
+    return "入力形式が違うよ。helpコマンドで確認してね。"
+  end
+
+  if (1..100).include?(number)
+    event_message_id = event.message.id
+    message_data = Discordrb::API::Channel.messages(@bot.token, event.channel.id, number, event_message_id)
+    message_ids = JSON.parse(message_data).map { |data| data["id"]}
+    if message_ids.size == 100
+      Discordrb::API::Channel.delete_message(@bot.token, event.channel.id, event_message_id)
+      Discordrb::API::Channel.bulk_delete_messages(@bot.token, event.channel.id, message_ids)
+    else
+      message_ids << event_message_id
+      Discordrb::API::Channel.bulk_delete_messages(@bot.token, event.channel.id, message_ids)
+    end
+  else
+    "指定できる個数は1~100個です。"
+  end
+end
+
+@bot.run
---
+++ たしゅぼっと/Code/commands_help.rb
@@ -0,0 +1,64 @@
+require 'discordrb'
+
+@bot = Discordrb::Commands::CommandBot.new(token: "NzMxMTExMjY4MjAyODQwMTE2.XwhSbQ.GVTdg4FrtJLW9RCoqgXYRxBLENw", client_id: 731111268202840116, prefix:'?')
+
+File.open('D:\ドキュメント\Programming\Project\Discord\Bot\たしゅぼっと\Code\body.rb', "r") do |file|
+  file_data = file.readlines
+  @commands_list = file_data.map { |datum| datum.match(/@bot.command :(.+) do |.+|/)[1] }.compact!
+  @commands_list_for_help = @commands_list.map { |command| "`#{command}`"}.join(", ")
+end
+
+def create_help_embed(channel_object, title = nil, attribute = nil, input_format_description = nil)
+  channel_object.send_embed do |embed|
+    embed.title = "コマンド名: #{title}"
+    embed.description = "種類: #{attribute[0]}\n説明:\n```#{attribute[1]}```"
+    embed.add_field(name: "入力形式", value: input_format_description, inline: false) if input_format_description # Discordrb::Webhooks::EmbedField.new でも可(このときは embed.push() になる。)
+  end
+end
+
+def commands_help(event_channel_object, command_name)
+  if command_name.nil?
+    return event_channel_object.send_embed do |embed|
+      embed.title = "Help"
+      embed.description = "たしゅぼっとで使えるコマンド一覧です。"
+      embed.add_field(name: "Commands", value: "#{@commands_list_for_help}", inline: false)
+      embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: "[?help コマンド名]で詳細を開けるよ")
+    end
+  end
+
+  if @commands_list.include?(command_name)
+    case command_name
+    when "help"
+      create_help_embed(
+        event_channel_object, "help", ["一般コマンド", "コマンド一覧を表示します。属性にコマンド名を指定すると、そのコマンドの詳細を確認できます。"], "[?help コマンド名]"
+     )
+    when "cmsg"
+      create_help_embed(
+        event_channel_object, "cmsg", ["一般コマンド", "属性にメッセージIDを指定すると、そのメッセージの内容を表示します。属性指定は必須です。"], "[?cmsg メッセージID]"
+      )
+    when "name"
+      create_help_embed(
+        event_channel_object, "name", ["一般コマンド", "discord上のユーザー名とこの鯖でのニックネームを表示します。"], "[?name]"
+      )
+    when "addm"
+      create_help_embed(
+        event_channel_object, "addm", ["一般コマンド", "?chomで提示される曲を追加できます。属性指定は必須です。"], "[?addm 曲名]"
+      )
+    when "chom"
+      create_help_embed(
+        event_channel_object, "chom", ["一般コマンド", "属性に曲数を指定すると、任意の曲を曲数だけ提示します。許可設定では、ユーザーによって追加された曲が選択されるかを変更可能です。曲数指定は必須です。許可設定はデフォルトでoffになっています。"], "[?name 曲数 (on/off)]"
+      )
+    when "dem"
+      create_help_embed(
+        event_channel_object, "dem", ["管理者コマンド", "属性に削除したいメッセージの数を指定すると、コマンドより前にあるメッセージをその数だけ削除します。(コマンド自体は数に含まれませんが、削除されます。)"], "[?dem メッセージ数]"
+      )
+    else
+      create_help_embed(
+        event_channel_object, command_name, ["？？？", "未実装または開発者用のコマンドだよ。"]
+      )
+
+    end
+  else
+    "#{command_name}というコマンドは存在しないよ！"
+  end
+end
